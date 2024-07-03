400860292ec1d526b3b3a27b8aa8894f4401438d
abc15018045126 <abc15018045126@gmail.com>
2024-07-03 21:31:28 +0800

    添加项目文件。

---
+++ Plugin.cs
@@ -0,0 +1,68 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Il2CppInterop.Common.Attributes;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.InteropTypes;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using Il2CppInterop.Runtime.Runtime;
+using Il2CppSystem;
+
+
+namespace Project8
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+
+    public class Plugin : BasePlugin
+    {
+        private static readonly System.IntPtr NativeFieldInfoPtr_autoBirthItem;
+
+        public unsafe int autoBirthItem2
+        {
+            get
+            {
+                nint num = (nint)IL2CPP.Il2CppObjectBaseToPtrNotNull((Il2CppObjectBase)(object)this) + (int)IL2CPP.il2cpp_field_get_offset(NativeFieldInfoPtr_autoBirthItem);
+                return *(int*)num;
+                Debug.WriteLine(num);
+            }
+            set
+            {
+                
+                *(int*)((nint)IL2CPP.Il2CppObjectBaseToPtrNotNull((Il2CppObjectBase)(object)this) + (int)IL2CPP.il2cpp_field_get_offset(NativeFieldInfoPtr_autoBirthItem)) = value;
+                int autoBirthItemvalue = value;
+                Debug.WriteLine(autoBirthItemvalue);
+            }
+        }
+
+
+
+
+        internal static new ManualLogSource Log;
+        public override void Load()
+        { }
+            // Plugin startup logic
+            public unsafe int autoBirthItem
+        {
+            get
+            {
+                nint num = (nint)IL2CPP.Il2CppObjectBaseToPtrNotNull((Il2CppObjectBase)(object)this) + (int)IL2CPP.il2cpp_field_get_offset(NativeFieldInfoPtr_autoBirthItem);
+                return *(int*)num;
+                Debug.WriteLine(num);
+            }
+            set
+            {
+
+                *(int*)((nint)IL2CPP.Il2CppObjectBaseToPtrNotNull((Il2CppObjectBase)(object)this) + (int)IL2CPP.il2cpp_field_get_offset(NativeFieldInfoPtr_autoBirthItem)) = value;
+                int autoBirthItemvalue = value;
+                Debug.WriteLine(autoBirthItemvalue);
+                autoBirthItemvalue = 1;
+                Debug.WriteLine(autoBirthItemvalue);
+            }
+        }
+    }
+}
+
